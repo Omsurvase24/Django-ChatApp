f2565b576cec69a6a87c766e07b724ec27f6537d
Omsurvase24 <omsurvase24@gmail.com>
2024-04-23 02:17:11 +0530

    final commit

--- room/admin.py
+++ room/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Room
+from .models import Room, Message
 
 admin.site.register(Room)
+admin.site.register(Message)
